8726c34acff73b90b345f65b0328fe2fca33ddbd
Soji <soji@Soji.local>
2021-01-18 15:23:47 +0900

    clickでスクロール

--- static/javascript/homepage.js
+++ static/javascript/homepage.js
@@ -1,3 +1,4 @@
+'use strict';
 const typewriter = (param) => {
     let el = document.querySelector(param.el);
     let speed = param.speed;
@@ -17,12 +18,56 @@ typewriter({
 });
 
 let loginDisplay = () =>{
-    const loginElem = document.getElementById('login');
+    const showElem = document.querySelectorAll('.is-show');
     document.addEventListener('keypress',(e) => {
         if(e.key === 'Enter') {
-            loginElem.classList.add('show');
+            showElem.forEach((item) => {
+            item.classList.add('show'); 
+            console.log(item)   
+            })
         }
     })
 }
 
 loginDisplay();
+
+let loopFactory = (func) => {
+    let i = 0;
+    let handler = {};
+    let loop = () => {
+        i = func(i);
+        window.scrollBy(0,i);
+        if (i < 31) {
+        handler.id = requestAnimationFrame(loop);
+        }
+    }
+    handler.id = requestAnimationFrame(loop);
+    return handler;
+}
+
+let increament = (i) => {
+    if (i > 30){
+        i === 30;
+    }else{
+        i += 1;
+    }
+    console.log(i);
+    return i;
+}
+
+let clickDisplay = () => {
+    const clickElem = document.getElementById('intro-title');    
+    clickElem.addEventListener('click', () => {
+        let interval = loopFactory(increament);
+    })
+}
+
+clickDisplay();
+
+window.addEventListener('scroll', () => {
+    const clickElem = document.getElementById('click');
+    if (window.scrollY > 230){
+        clickElem.classList.remove('flash');
+        clickElem.classList.add('not-flash');
+    }
+})
